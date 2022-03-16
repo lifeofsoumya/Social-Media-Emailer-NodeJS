d17d200bdec10ccf29a70ce319be311f0553d0a6
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-03-16 09:45:27 +0530

    tried page.$$ to get all elements with class

--- app.js
+++ app.js
@@ -6,32 +6,33 @@ const app = express();
 
 var instUrl = 'https://www.instagram.com/lifeofsoumya' // defining the scrape-able url
 
-
 async function scrapeChannel(url) { // init function with to be scraped url argument
 
     const browser = await puppeteer.launch();      // launch puppeteer
     const page = await browser.newPage();       // generate a headless browser
     await page.goto(url);      // open argument passed url
 
-    const countInDiv = await page.evaluate(querySelectorAll('.v1Nh3, .kIKUG').length) // using the classes that every post contains, to count number of posts
-    console.log(`post count is ${countInDiv}`) 
+    const postCount = await page.$$('.v1Nh3');  // using the classes that every post contains, to count number of posts
+        console.log(`post count is ${postCount}`)
 
     const imageHref = await page.evaluate(() => { // function to get link of profile picture
         return document.querySelector('._6q-tv').getAttribute('src').replace('/', '')
     })
 
     const profPic = await page.goto(instUrl + imageHref)  // creates profile picture image url
+    console.log(`link of profile picture is ${profPic}`)
 
-    const buffer = await profPic.buffer()
-    await writeFileAsync(path.join(__dirname, 'profPic.png'), buffer) // saves the profile picture
+    // const buffer = await profPic.buffer()
+    // await writeFileAsync(path.join(__dirname, 'profPic.png'), buffer) // saves the profile picture
 }
 
 
 scrapeChannel(instUrl)
 
 
+
 app.get('/', (req, res)=>{
-    res.send('Project init')
+    res.send('Working')
 })
 
 
